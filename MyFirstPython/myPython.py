b5800aaa11a280cbce28ff86df6d780d0e7ec7d6
Katerina <emrodionova@bk.ru>
2023-03-03 21:06:16 +0300

    добавила новый репозиторий python

---
+++ MyFirstPython/myPython.py
@@ -0,0 +1,20 @@
+# Пузырьковая сортировка от меньшего к большему
+
+from random import randint
+
+N = 30
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+
+i = 0
+while i < N - 1:
+    j = 0
+    while j < N - 1 - i:
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+        j += 1
+    i += 1
+
+print(a)  # вывод на печать
