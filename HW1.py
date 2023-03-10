587946c1d44f848ace7fb89f83610800a3bb2bd5
Katerina <emrodionova@bk.ru>
2023-03-10 19:45:48 +0300

    added HW sem1

---
+++ HW1.py
@@ -0,0 +1,14 @@
+"""
+Задача 2. Найдите сумму цифр
+трехзначного числа
+"""
+number = int(input('Введите трехзначное число: '))
+
+sum = 0
+temp = 0
+while number > 0:
+    temp = number % 10
+    sum += temp
+    number = number // 10
+
+print(sum)
